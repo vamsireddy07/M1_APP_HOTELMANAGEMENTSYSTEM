34173653558c1b76a0c162e0bde0140cff6adae0
vamsireddy07 <48744179+vamsireddy07@users.noreply.github.com>
2021-11-21 15:40:47 +0530

    Add files via upload

---
+++ 3_Implementation/inc/details.h
@@ -0,0 +1,22 @@
+/**
+ * @file details.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef DETAILS_H
+#define DETAILS_H
+
+void add();  
+void list();
+void edit();  
+void delete1();
+void search();
+void login();
+
+#endif
